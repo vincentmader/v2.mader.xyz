f823dfc45c481818eec3d2b5bc1931aef49c3c7d
Vincent Mader <vincent.mader@icloud.com>
2020-09-30 02:09:24 +0200

    added test template to load from __init__

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return 'Hello world! :)'
+    return render_template('./templates/test.html')
 
 
 if __name__ == '__main__':
