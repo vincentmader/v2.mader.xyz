08dcb8068f2867ae976f89aca4824fc1d0e448e1
vincentmader <vincent.mader@icloud.com>
2020-10-01 19:39:48 +0200

    added second page with rudimentary game

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import flask
+from flask import Flask, render_template, send_from_directory
 
 
 app = Flask(__name__)
@@ -7,6 +8,16 @@ app = Flask(__name__)
 @app.route('/')
 def chart():
     return render_template('chart.html', **{'ayy': 'ayy'})
+
+
+@app.route('/game/')
+def game():
+    # style_sheet = send_from_directory('templates', 'style.css')
+    # game = send_from_directory('templates', 'script.js')
+    # print(game)
+
+    # return render_template('game.html', game=game, style_sheet=style_sheet)
+    return render_template('game.html')
 
 
 if __name__ == '__main__':
