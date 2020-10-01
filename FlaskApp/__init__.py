cb87f7d5bd9c1799a58d59110b9a76efd16953b7
Vincent Mader <vincent.mader@icloud.com>
2020-10-01 20:23:56 +0000

    changed __init__.py to make game thingy the index site

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -5,12 +5,12 @@ from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 
+# @app.route('/')
+# def chart():
+#     return render_template('chart.html', **{'ayy': 'ayy'})
+
+
 @app.route('/')
-def chart():
-    return render_template('chart.html', **{'ayy': 'ayy'})
-
-
-@app.route('/game/')
 def game():
     # style_sheet = send_from_directory('templates', 'style.css')
     # game = send_from_directory('templates', 'script.js')
