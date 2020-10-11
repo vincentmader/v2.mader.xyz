51872d60272143fc7d8c085a6aba57667c9b1b62
vincentmader <vincent.mader@icloud.com>
2020-10-12 00:52:01 +0200

    added function for creating example timeseries data

--- FlaskApp/chronos/stats/time_series/example_data.py
+++ FlaskApp/chronos/stats/time_series/example_data.py
@@ -4,11 +4,13 @@ from datetime import datetime as dt
 from datetime import timedelta as td
 
 
-def main():
+def main(nr_of_datapoints=400):
+
     data = []
-    for i in range(400):
+    for i in range(nr_of_datapoints):
         data.append({
             'timestamp': (dt.now() - td(days=i)).timestamp(),
             'value': random.randint(0, 10)
         })
+
     return data
