9f97acfa2526d4a734e8e49c4fa5974c3639f0a0
vincentmader <vincent.mader@icloud.com>
2020-10-12 03:05:05 +0200

    minor

--- FlaskApp/static/js/physics_sims/pendulum/single.js
+++ FlaskApp/static/js/physics_sims/pendulum/single.js
@@ -3,15 +3,17 @@ const π = Math.PI;
 const dt = 1;
 const L = 200;
 const g = 1;
+const R = 5;
 
 const W = 600;
 const H = 600;
 
 function Pendulum(o_x, o_y, φ) {
-  this.o_x = o_x; // coords of pivot origin
+  // set coords of pivot origin
+  this.o_x = o_x;
   this.o_y = o_y;
-
-  this.φ = φ; // angular coords of pendulum mass
+  // set angular coords of pendulum mass
+  this.φ = φ;
   this.ω = 0;
   this.α = 0;
 
@@ -29,7 +31,7 @@ function Pendulum(o_x, o_y, φ) {
     ctx.strokeStyle = "white";
     ctx.fillStyle = "white";
     // draw mass
-    ctx.arc(p_x + W / 2, p_y + H / 2, 10, 0, 2 * Math.PI);
+    ctx.arc(p_x + W / 2, p_y + H / 2, R, 0, 2 * Math.PI);
     ctx.stroke();
     ctx.fill();
     // draw rod
@@ -40,13 +42,12 @@ function Pendulum(o_x, o_y, φ) {
 }
 
 const init = () => {
-  canvas = document.getElementById("canvas2");
+  canvas = document.getElementById("square_canvas");
   ctx = canvas.getContext("2d");
   canvas.width = W;
   canvas.height = H;
 
-  p = new Pendulum(0, 0, π / 2);
-
+  p = new Pendulum(0, 0, π / 3);
   setInterval(function () {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     p.draw();
