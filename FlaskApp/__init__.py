8758072d5059469223832c10382bfb7ef239f7e8
Vincent C. Mader <vincent.mader@icloud.com>
2021-01-26 16:23:16 +0100

    updated __init__

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,12 +1,17 @@
+import json
 import os
 import sys
 
 import flask
 from flask import Flask, render_template, send_from_directory, request
+import numpy as np
 
 import chronos
+from chronos import plots, stats
 import config
 from config import PATH_TO_STATIC
+
+import comp_phys
 
 
 # initialize app
@@ -40,32 +45,82 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-    nav_grid_items = [
-        {'id': 'pyplots', 'link': '/chronos/test_pyplots'},
-        {'id': 'test', 'link': '/chronos/test'},
-        {'id': 'boltzmann', 'link': '/comp_phys/boltzmann'},
-        {'id': 'orbit'},
-        {'id': 'solar'},
-        {'id': 'tatooine'},
-        {'id': 'solar'},
-        {'id': 'tatooine'},
+    nav_grid_sections = [
+        [
+            {'id': 'double_pendulum', 'link': '/comp_phys/double_pendulum'},
+            {'id': 'n_body', 'link': '/comp_phys/n_body'},
+            {'id': 'gas_in_a_box'},
+            # {'id': 'bokeh'},
+            {'id': 'lissajous'},
+            {'id': 'bachelor_thesis'},
+            # {'id': 'spotify'},
+            # {'id': 'pyplot'},
+            # {'id': 'factfulness'},
+            {'id': 'boltzmann', 'link': '/comp_phys/boltzmann'},
+        ], [
+            {'id': 'testing_bokeh', 'link': '/chronos/testing/bokeh'},
+            {'id': 'testing_chartjs', 'link': '/chronos/testing/chartjs'},
+            {'id': 'testing_pyplot', 'link': '/chronos/testing/pyplots'},
+            # {'id': 'orbit'},
+            # {'id': 'solar'},
+            # {'id': 'tatooine'},
+            # {'id': 'solar'},
+            # {'id': 'tatooine'},
+        ]
     ]
-    enumerated_nav_grid_items = enumerate(nav_grid_items)
 
     return render_template(
         'index.html',
-        enumerated_nav_grid_items=enumerated_nav_grid_items,
+        nav_grid_sections=nav_grid_sections,
         nr_of_cols=4
     )
 
 
-@app.route('/comp_phys/boltzmann')
-def boltzmann2():
-    return render_template('comp_phys/layout1.html')
+@app.route('/chronos/testing/chartjs')
+def testing_chartjs():
+
+    return render_template('chronos/testing/chartjs.html')
 
 
-@app.route('/chronos/test_pyplots')
-def pyplots():
+@app.route('/comp_phys/double_pendulum')
+def comp_phys_double_pendulum():
+
+    # th_1, th_2, p_1, p_2 = 0, 0, 1, 1
+    # y0 = [th_1, th_2, p_1, p_2]
+
+    # system_states = comp_phys.double_pendulum.integrate.main()
+    system_states = np.loadtxt('./comp_phys/double_pendulum/out/ys.txt')
+    system_states = [list(i) for i in system_states]
+    # print(system_states)
+
+    props = {
+        'title': 'double pendulum',
+        'description': 'theory, latex...',
+        'ys': json.dumps(system_states),
+    }
+
+    return render_template('comp_phys/layout1.html', props=props)
+
+
+@app.route('/comp_phys/n_body')
+def comp_phys_n_body():
+
+    system_states = np.loadtxt('./comp_phys/n_body/out/ys.txt')
+    system_states = [list(i) for i in system_states]
+    props = {
+        'ys': json.dumps(system_states),
+    }
+
+    return render_template('comp_phys/n_body.html', props=props)
+
+
+# @app.route('/comp_phys/double_pendulum')
+# def comp_phys_double_pendulum_get():
+#     return [1, 3, 4]
+
+
+@app.route('/chronos/testing/pyplot')
+def pyplot():
 
     images = []
 
@@ -78,19 +133,49 @@ def pyplots():
     print(images)
 
     return render_template(
-        'chronos/test_pyplots.html',
+        'chronos/testing/pyplot.html',
         images=images
     )
 
 
-@app.route('/chronos/test')
-def test():
+@app.route('/chronos/testing/bokeh')
+def testing_bokeh(lolol='Test'):
 
-    script, div = chronos.plots.bokeh.google_takeout.gmap()
-    # script, div = chronos.plots.bokeh.google_takeout.bar_plot_search_hits(
-    #     'youtube')
+    plot_functions = [
+        plots.bokeh.google_takeout.gmap,
+    ]
 
-    return render_template('chronos/test.html', script=script, div=div)
+    scripts, divs = [], []
+    for f in plot_functions:
+        script, div = f()
+        scripts.append(script)
+        divs.append(div)
+
+    script, div = plots.bokeh.google_takeout.bar_plot_search_hits('youtube')
+    scripts.append(script)
+    divs.append(div)
+
+    x, y = stats.time_series.avg_grades()
+    # x, y = np.array(list(foo.keys())), np.array(list(foo.values()))
+    # x, y = stats.time_series.avg_grades()
+    script, div = plots.bokeh.basic.time_series(x, y)
+    scripts.append(script)
+    divs.append(div)
+
+    # x, y = stats.time_series.chars_written_in_daily_log()
+    # x, y = np.array(list(foo.keys())), np.array(list(foo.values()))
+    # x, y = stats.time_series.avg_grades()
+    # script, div = plots.bokeh.basic.time_series(x, y)
+    # scripts.append(script)
+    # divs.append(div)
+
+    return render_template('chronos/testing/bokeh.html', scripts=scripts, divs=divs)
+
+
+@app.route('/chronos/testing/bokeh', methods=['POST'])
+def testing_bokeh_post():
+    textfield_1 = request.form['textfield_1']
+    return test(textfield_1)
 
 
 # old
@@ -156,7 +241,6 @@ def chart_test():
     return render_template('chart_test.html')
 
 
-# bokeh
 @app.route('/bokeh/')
 def bokeh():
     scripts, divs = [], []
@@ -170,25 +254,25 @@ def bokeh_post():
 
     scripts, divs = [], []
     if plot_name == 'sleep':
-        script, div = chronos.plots.bokeh.sleep_analysis.sleep_snake()
+        script, div = plots.bokeh.sleep_analysis.sleep_snake()
         scripts.append(script)
         divs.append(div)
     elif plot_name == 'map':
-        script, div = chronos.plots.bokeh.various.location_history()
+        script, div = plots.bokeh.various.location_history()
         scripts.append(script)
         divs.append(div)
     elif plot_name == 'grades':
-        script, div = chronos.plots.bokeh.various.grades()
+        script, div = plots.bokeh.various.grades()
         scripts.append(script)
         divs.append(div)
     elif plot_name == 'heatmap':
         # data = chronos.stats.sample_data()
-        data = chronos.stats.time_series.daily_log.chars_written_in_daily_log()
-        script, div = chronos.plots.bokeh.basic.heatmap(data, 'daily')
+        data = stats.time_series.daily_log.chars_written_in_daily_log()
+        script, div = plots.bokeh.basic.heatmap(data, 'daily')
         scripts.append(script)
         divs.append(div)
     elif plot_name == 'altitude':
-        script, div = chronos.plots.bokeh.various.altitude_history()
+        script, div = plots.bokeh.various.altitude_history()
         scripts.append(script)
         divs.append(div)
 
@@ -208,7 +292,7 @@ def messages_post():
     scripts, divs = [], []
     query = request.form['text']
 
-    script, div = chronos.plots.bokeh.various.messages(query)
+    script, div = plots.bokeh.various.messages(query)
     scripts.append(script)
     divs.append(div)
 
--- flaskapp.wsgi
+++ flaskapp.wsgi
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from dotenv import load_dotenv
 from FlaskApp import app as application
 import os
 import sys
@@ -7,7 +8,6 @@ import logging
 sys.path.insert(0, '/var/www/FlaskApp')
 logging.basicConfig(stream=sys.stderr)
 
-from dotenv import load_dotenv
 load_dotenv()
 
 application.secret_key = os.getenv('$SECRET_KEY', 'for dev')
