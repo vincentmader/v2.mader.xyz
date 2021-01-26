10646c95a39d936599e4428bff3c35f81b834f4f
Vincent C. Mader <vincent.mader@icloud.com>
2021-01-26 20:12:08 +0100

    updated paths for new machine

--- FlaskApp/config.py
+++ FlaskApp/config.py
@@ -1,8 +1,10 @@
 import os
 
 
-PATH_TO_PROJECT = '/home/vinc/Documents/mader.xyz/FlaskApp/chronos/'
-PATH_TO_RAW_DATA = '/home/vinc/Documents/chronos_data/'
+PATH_TO_PROJECT = '/home/vinc/code/mader.xyz/FlaskApp/chronos/'
+PATH_TO_STATIC = '/home/vinc/code/mader.xyz/FlaskApp/static/'
+PATH_TO_RAW_DATA = '/home/vinc/docs/chronos_data/'
+PATH_TO_DAILY_LOGS = '/home/vinc/org/journal'
 
 GSPREAD_CREDS_SETUP = os.path.exists(
     os.path.join(PATH_TO_RAW_DATA, 'creds', 'gspread_creds.json')
