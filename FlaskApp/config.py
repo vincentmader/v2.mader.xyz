2b76a5d788fab6067b0abd66e2d22fb00fc2769a
Vincent C. Mader <vincent.mader@icloud.com>
2021-02-23 04:12:05 +0100

    minor

--- FlaskApp/config.py
+++ FlaskApp/config.py
@@ -1,7 +1,8 @@
 import os
 
 
-PATH_TO_PROJECT = '/home/vinc/code/mader.xyz/FlaskApp/chronos/'
+PATH_TO_PROJECT = '/home/vinc/code/mader.xyz/FlaskApp/'
+PATH_TO_CHRONOS = '/home/vinc/code/mader.xyz/FlaskApp/chronos'
 PATH_TO_STATIC = '/home/vinc/code/mader.xyz/FlaskApp/static/'
 PATH_TO_RAW_DATA = '/home/vinc/docs/chronos_data/'
 PATH_TO_DAILY_LOGS = '/home/vinc/org/journal'
