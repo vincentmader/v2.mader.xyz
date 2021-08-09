fac93d71f107b5d4acda54fd327ee84d2b1e2628
Vincent C. Mader <vincent.mader@icloud.com>
2021-08-09 15:42:04 +0200

    implemented basic React frontend, sent from flask backend. nice!

---
+++ FlaskApp/static/js/react_frontend/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+};
