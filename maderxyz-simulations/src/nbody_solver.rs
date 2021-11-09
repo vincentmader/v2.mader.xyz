cb4bae823fdc34bdc6a190aa60705e06c0ba6ca8
Vincent Mader <vincent.mader@icloud.com>
2021-11-09 01:28:08 +0100

    minor

---
+++ maderxyz-simulations/src/nbody_solver.rs
@@ -0,0 +1,14 @@
+// use wasm_bindgen::prelude::*;
+
+pub struct MoonSim {}
+
+impl MoonSim {
+
+    pub fn update(&self) { }
+
+    pub fn new() -> MoonSim {
+        MoonSim {
+
+        }
+    }
+}
