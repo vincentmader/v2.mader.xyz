4b5ec0fc0e538f99982eafb7b21643ada48ce875
Vincent C. Mader <vincent.mader@icloud.com>
2023-04-24 10:06:47 +0200

    fix: updated toolchain

--- website/mxyz-server/src/lib.rs
+++ website/mxyz-server/src/lib.rs
@@ -1,12 +1,13 @@
 #[macro_use]
 extern crate rocket;
 use rocket::fs::{relative, FileServer};
+use rocket::{Ignite, Rocket};
 use rocket_dyn_templates::Template;
 
 mod views;
 
 #[rocket::main]
-pub async fn main() -> Result<(), rocket::Error> {
+pub async fn main() -> Result<Rocket<Ignite>, rocket::Error> {
     let file_server = FileServer::from(relative!("../mxyz-server/static"));
     let routes = routes![views::index::route, views::routes,];
 
